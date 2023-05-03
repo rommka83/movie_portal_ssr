d99f2df2dcad8830312d749dba0c78e99a74ed5c
Роман Нарожнов <rommka_83@mail.ru>
2023-05-03 13:54:56 +0300

    добавил модальное окно с коментарием на коментарий

--- src/entities/OneComment/OneComment.tsx
+++ src/entities/OneComment/OneComment.tsx
@@ -1,9 +1,12 @@
-import React, { FC, HTMLAttributes } from 'react';
+import React, { FC, HTMLAttributes, useState } from 'react';
 import styles from './onecomment.module.css';
 import classNames from 'classnames';
 import UserPhoto from 'shared/user/UserPhoto';
 import { IOneComment } from 'shared/types/IOneComment';
 import { useTranslation } from 'react-i18next';
+import { Modal } from 'shared/ui/Modal';
+import { OneCommentHeader } from 'widgets/OneCommentHeader';
+import { OneCommentBody } from 'widgets/OneCommentBody';
 
 interface IProps {
   comment: IOneComment;
@@ -11,36 +14,47 @@ interface IProps {
 
 export const OneComment: FC<HTMLAttributes<HTMLDivElement> & IProps> = ({ className, comment }) => {
   const { t } = useTranslation();
-  const name = comment.author.split(' ').slice(0, 1);
-  const surName = comment.author.split(' ').slice(1);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
 
   return (
-    <article className={classNames(styles.root, className)}>
-      <div className={styles.head}>
-        <UserPhoto />
-        <div className={styles.author}>
-          <p className={styles.name}>{name}</p>
-          <p className={styles.name}>{surName}</p>
+    <>
+      <article className={classNames(styles.root, className)}>
+        <OneCommentHeader comment={comment} />
+        <OneCommentBody comment={comment} />
+        <div className={styles.commentFooter}>
+          <button
+            className={classNames(styles.commentBtn, styles.open, 'icon-message_20__0')}
+            onClick={() => setModalIsOpen(true)}
+          >
+            <span>{t('sectionTitle.AddComment')}</span>
+          </button>
+          <button className={classNames(styles.commentBtn, styles.like, 'icon-thumbUp_16__0')}>
+            <span>{t('sectionTitle.Healthy')}</span>
+            <span>20</span>
+          </button>
+          <button className={classNames(styles.commentBtn, styles.like, 'icon-thumbDown_16__0')}>
+            <span>{t('sectionTitle.No')}</span>
+            <span>5</span>
+          </button>
         </div>
-        <p className={styles.data}>{new Date(comment.date).toLocaleDateString()}</p>
-      </div>
-      <div className={styles.commentBody}>
-        <h5 className={styles.commentTitle}>{comment.title}</h5>
-        <p className={styles.commentContent}>{comment.review}</p>
-      </div>
-      <div className={styles.commentFooter}>
-        <button className={classNames(styles.commentBtn, styles.open, 'icon-message_20__0')}>
-          <span>{t('sectionTitle.AddComment')}</span>
-        </button>
-        <button className={classNames(styles.commentBtn, styles.like, 'icon-thumbUp_16__0')}>
-          <span>{t('sectionTitle.Healthy')}</span>
-          <span>20</span>
-        </button>
-        <button className={classNames(styles.commentBtn, styles.like, 'icon-thumbDown_16__0')}>
-          <span>{t('sectionTitle.No')}</span>
-          <span>5</span>
-        </button>
-      </div>
-    </article>
+      </article>
+      {modalIsOpen && (
+        <Modal>
+          <div className={classNames(styles.modal, 'container')}>
+            <OneCommentHeader comment={comment} className={styles.modalHead} />
+            <OneCommentBody comment={comment} className={styles.modalComment} />
+            <textarea className={styles.myComment} />
+            <div className={styles.modalBlockBtn}>
+              <button onClick={() => {}} className={styles.modalBtn}>
+                Комментировать
+              </button>
+              <button onClick={() => setModalIsOpen(false)} className={styles.modalBtn}>
+                Закрыть
+              </button>
+            </div>
+          </div>
+        </Modal>
+      )}
+    </>
   );
 };
---
+++ src/widgets/OneCommentHeader/OneCommentHeader.tsx
@@ -0,0 +1,29 @@
+import React, { FC, HTMLAttributes } from 'react';
+import styles from './onecommentheader.module.css';
+import UserPhoto from 'shared/user/UserPhoto';
+import classNames from 'classnames';
+import { comment } from 'stories/OneComment.stories';
+import { IOneComment } from 'shared/types/IOneComment';
+
+interface IProps {
+  comment: IOneComment;
+}
+
+export const OneCommentHeader: FC<HTMLAttributes<HTMLDivElement> & IProps> = ({
+  comment,
+  className,
+}) => {
+  const name = comment.author.split(' ').slice(0, 1);
+  const surName = comment.author.split(' ').slice(1);
+
+  return (
+    <div className={classNames(styles.head, className)}>
+      <UserPhoto />
+      <div className={styles.author}>
+        <p className={styles.name}>{name}</p>
+        <p className={styles.name}>{surName}</p>
+      </div>
+      <p className={styles.data}>{new Date(comment.date).toLocaleDateString()}</p>
+    </div>
+  );
+};
