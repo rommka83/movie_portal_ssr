6d79c0b2597b5fdc2b446af8e70642e4a1ea4e4e
Роман Нарожнов <rommka_83@mail.ru>
2023-05-16 20:28:59 +0300

    переделал главный слайдер и удалил swiper

--- src/widgets/PromoSlider/PromoSlider.tsx
+++ src/widgets/PromoSlider/PromoSlider.tsx
@@ -1,43 +1,137 @@
-import React from 'react';
-
+import React, { useEffect, useMemo, useRef, useState } from 'react';
 import styles from './promoSlider.module.css';
-import 'swiper/css';
-import 'swiper/css/navigation';
-
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Autoplay, Navigation } from 'swiper';
-
 import { PromoSlide } from 'entities/PromoSlide';
-import { IFilm } from 'shared/types/IFilm';
 import { UseMedia } from 'shared/hooks/useMedia';
+import DB from '../../../static/promo_DB.json';
+import classNames from 'classnames';
 
-type PropsType = {
-  movies: IFilm[];
-};
+export const PromoSlider = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const tablet = UseMedia('(max-width:800px)');
+  const container = useRef<HTMLDivElement>(null);
+  const [cardWidth, setCardWidth] = useState(0);
+  const [scrollLeft, setScrollLeft] = useState(false);
+  const [scrollRight, setScrolRight] = useState(false);
 
-export const PromoSlider: React.FC<PropsType> = ({ movies }) => {
-  const brakePoint = UseMedia('(max-width:768px)');
+  const { prevImgIndex, nextImgIndex, lastImgIndex } = useMemo(() => {
+    let prevImgIndex;
+    let nextImgIndex;
+    let lastImgIndex;
+
+    prevImgIndex = activeIndex ? activeIndex - 1 : DB.length - 1;
+    nextImgIndex = activeIndex === DB.length - 1 ? 0 : activeIndex + 1;
+
+    if (activeIndex === DB.length - 1) {
+      lastImgIndex = 1;
+    } else if (activeIndex === DB.length - 2) {
+      lastImgIndex = 0;
+    } else lastImgIndex = activeIndex + 2;
+
+    return { prevImgIndex, nextImgIndex, lastImgIndex };
+  }, [activeIndex]);
+
+  const hendleScrollLeft = function () {
+    setActiveIndex((current) => {
+      const res = current === DB.length - 1 ? 0 : current + 1;
+      return res;
+    });
+  };
+  const hendleScrollRight = function () {
+    setActiveIndex((current) => {
+      const res = current ? current - 1 : DB.length - 1;
+      return res;
+    });
+  };
+
+  const sliderHight = useMemo(() => {
+    return tablet ? cardWidth * 1.45 : cardWidth * 0.45;
+  }, [tablet, cardWidth]);
+
+  useEffect(() => {
+    if (container.current === null) return;
+    const widthContainer = container.current;
+    const resizeObserver = new ResizeObserver((entries) => {
+      for (const entry of entries) {
+        if (entry.contentRect.width !== cardWidth) {
+          setCardWidth(entry.contentRect.width);
+        }
+      }
+    });
+    resizeObserver.observe(container.current);
+
+    let interval = setInterval(() => {
+      setScrollLeft(false);
+      hendleScrollLeft();
+    }, 8000);
+
+    if (scrollLeft) {
+      clearInterval(interval);
+      setScrollLeft(false);
+      hendleScrollRight();
+      interval = setInterval(() => {
+        hendleScrollLeft();
+      }, 8000);
+    }
+
+    if (scrollRight) {
+      clearInterval(interval);
+      setScrolRight(false);
+      hendleScrollLeft();
+      interval = setInterval(() => {
+        hendleScrollLeft();
+      }, 8000);
+    }
+
+    return () => {
+      widthContainer && resizeObserver.unobserve(widthContainer);
+      clearInterval(interval);
+    };
+  }, [cardWidth, scrollLeft, scrollRight]);
 
   return (
-    <Swiper
-      modules={[Navigation, Autoplay]}
-      spaceBetween={20}
-      slidesPerView={1.25}
-      navigation={!brakePoint}
-      loop
-      centeredSlides
-      autoplay={{
-        delay: 8000,
-        disableOnInteraction: false,
-        pauseOnMouseEnter: true,
-      }}
-      className={styles.slider}
-    >
-      {movies.map((movie) => (
-        <SwiperSlide key={movie.id} className={styles.slide}>
-          <PromoSlide movie={movie} />
-        </SwiperSlide>
-      ))}
-    </Swiper>
+    <section className={styles.root}>
+      <div className={classNames(styles.slider, 'container')} style={{ height: `${sliderHight}px` }} ref={container}>
+        <div key={prevImgIndex} className={classNames(styles.sliderImg, styles.sliderImgPrev)}>
+          <PromoSlide
+            title={DB[prevImgIndex].title}
+            picture={tablet ? DB[prevImgIndex].pictureMobile : DB[prevImgIndex].picture}
+            description={DB[prevImgIndex].description}
+            width={cardWidth}
+          />
+        </div>
+        <div key={activeIndex} className={classNames(styles.sliderImg, styles.currentSlideImg)}>
+          <PromoSlide
+            title={DB[activeIndex].title}
+            picture={tablet ? DB[activeIndex].pictureMobile : DB[activeIndex].picture}
+            description={DB[activeIndex].description}
+            width={cardWidth}
+          />
+        </div>
+        <div key={nextImgIndex} className={classNames(styles.sliderImg, styles.sliderImgNext)}>
+          <PromoSlide
+            title={DB[nextImgIndex].title}
+            picture={tablet ? DB[nextImgIndex].pictureMobile : DB[nextImgIndex].picture}
+            description={DB[nextImgIndex].description}
+            width={cardWidth}
+          />
+        </div>
+        <div key={lastImgIndex} className={classNames(styles.sliderImg, styles.sliderImgLast)}>
+          <PromoSlide
+            title={DB[lastImgIndex].title}
+            picture={tablet ? DB[lastImgIndex].pictureMobile : DB[lastImgIndex].picture}
+            description={DB[lastImgIndex].description}
+            width={cardWidth}
+          />
+        </div>
+        <div
+          className={classNames(styles.btnControls, styles.btnControlsLeft, 'icon-arrowLeft_12x32__0')}
+          onClick={() => setScrollLeft(true)}
+        ></div>
+        <div
+          className={classNames(styles.btnControls, styles.btnControlsRight, 'icon-arrowRight_12x32__0')}
+          onClick={() => setScrolRight(true)}
+        ></div>
+      </div>
+    </section>
   );
 };
